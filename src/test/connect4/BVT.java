d404dc6a2e8501c44bf20331a10b5fd177095ab1
Penistrong <770560618@qq.com>
2020-04-01 11:56:44 +0800

    Writing BoardTest...incompleted

---
+++ src/test/connect4/BVT.java
@@ -0,0 +1,9 @@
+package connect4;
+/**
+ * 
+ * @author Penistrong
+ * @description used for categories
+ */
+public interface BVT {
+
+}
--- src/test/connect4/board/BoardTest.java
+++ src/test/connect4/board/BoardTest.java
@@ -1,5 +1,34 @@
 package connect4.board;
 
-public class BoardTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import connect4.BVT;
+
+public class BoardTest {
+	
+	Board testobj;
+	
+	@Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    @Category(BVT.class)
+    @Test
+    public void dropPieceMinTotalLock1() {
+    	testobj = new Board(6,7);
+    	//TODO: write test cases according to class Board's logic
+    }
+    
+    @Category(BVT.class)
+    @Test
+    public void dropPieceMinTotalLock2() {
+    	testobj = new Board(6,7);
+    }
 }
