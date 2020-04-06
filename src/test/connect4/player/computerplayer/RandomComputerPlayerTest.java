76529ab2cf4325982394a725f9b1a3018cd6bb8c
Penistrong <770560618@qq.com>
2020-04-06 15:07:09 +0800

    Initialize RandomComputerTest.class...

--- src/test/connect4/player/computerplayer/RandomComputerPlayerTest.java
+++ src/test/connect4/player/computerplayer/RandomComputerPlayerTest.java
@@ -1,5 +1,27 @@
 package connect4.player.computerplayer;
 
-public class RandomComputerPlayerTest {
+import static org.junit.Assert.fail;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RandomComputerPlayerTest {
+	RandomComputerPlayer testobj;
+	
+	@Before
+    public void setUp() throws Exception {
+		testobj = new RandomComputerPlayer();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    	//释放对象
+    	testobj = null;
+    }
+    
+    @Test
+    public void test() {
+    	fail("test method incompleted...");
+    }
 }
