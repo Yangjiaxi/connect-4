42fcde801a9e43415da892f3fc3cc70fe66ecad8
aoki <932877796@qq.com>
2020-04-06 16:18:48 +0800

    <MOD> add getter of board

---
+++ src/test/connect4/player/AgentCategoryTest.java
@@ -0,0 +1,16 @@
+package connect4.player;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+
+
+import connect4.BVT;
+import connect4.EC;
+import connect4.IPT;
+import connect4.LC;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory({BVT.class, EC.class})
+@Categories.ExcludeCategory({IPT.class, LC.class})
+public class AgentCategoryTest {
+}
