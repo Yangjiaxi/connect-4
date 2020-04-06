d94eeae9fddaa1f1db2de7e9a0a230fc3b09da5a
TianYize <wutangc1an@outlook.com>
2020-04-07 00:46:59 +0800

    RandomComputerPlayerTest 4 TCs added

--- src/test/connect4/player/computerplayer/RandomComputerPlayerTest.java
+++ src/test/connect4/player/computerplayer/RandomComputerPlayerTest.java
@@ -2,9 +2,12 @@ package connect4.player.computerplayer;
 
 import static org.junit.Assert.fail;
 
+import connect4.BVT;
+import connect4.EC;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class RandomComputerPlayerTest {
 	RandomComputerPlayer testobj;
@@ -20,8 +23,32 @@ public class RandomComputerPlayerTest {
     	testobj = null;
     }
     
+    @Category({EC.class, BVT.class})
     @Test
-    public void test() {
-    	fail("test method incompleted...");
+    public void testAskNextMinv1() {
+        //空盘时下一子随机出现在可能的7个位置中的1个
+
     }
+
+    @Category({BVT.class})
+    @Test
+    public void testAskNextMinv2() {
+        //落一子时下一子随机出现在可能的7个位置中的1个
+
+    }
+
+    @Category({BVT.class})
+    @Test
+    public void testAskNextMaxv1() {
+        //剩一子时下一子出现在所剩的1个位置
+
+    }
+
+    @Category({BVT.class})
+    @Test
+    public void testAskNextMaxv2() {
+        //满盘时下一子不出现
+
+    }
+
 }
