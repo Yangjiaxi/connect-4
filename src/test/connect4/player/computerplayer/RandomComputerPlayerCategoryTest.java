2319a25b74df8ae0d4e369fb792db8b6087be01b
TianYize <wutangc1an@outlook.com>
2020-04-07 14:45:59 +0800

    <ADD> RandomComputerPlayerCategoryTest and RandomComputerPlayerTestRunner

---
+++ src/test/connect4/player/computerplayer/RandomComputerPlayerCategoryTest.java
@@ -0,0 +1,17 @@
+package connect4.player.computerplayer;
+import connect4.BVT;
+import connect4.EC;
+import connect4.IPT;
+import connect4.LC;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory({BVT.class, EC.class})
+@Categories.ExcludeCategory({IPT.class, LC.class})
+@Suite.SuiteClasses({RandomComputerPlayerTest.class})
+public class RandomComputerPlayerCategoryTest {
+
+}
